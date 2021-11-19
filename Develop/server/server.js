1951f4e81d3f382cd59cd5fb331e1070c8b4364c
Julian Wilson <58565920+juju669@users.noreply.github.com>
2021-11-19 14:10:32 -0500

    added apollo server to server

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -8,6 +8,14 @@ const {ApolloServer} =require('apollo-server-express');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+});
+
+server.applyMiddleware({app});
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -16,8 +24,16 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+//app.use(routes);
+
+app.get('*',(req,res) => {
+  res.sendFile(path.join(_dirname, '../client/build/index.html'));
+});
+
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+  console.log(`API server running on port ${PORT}!`);
+  console.log(`Use Graphql at http://localhost${PORT}${server.graphqlPath}`);
 });
+})
